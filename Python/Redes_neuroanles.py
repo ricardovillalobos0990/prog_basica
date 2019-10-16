17566943abf70b3598d6fa3ea5d6094ac9d0fdd2
Ricardo Villalobos <ricardovillalobos0990@gmail.com>
2019-10-16 15:54:21 -0500

    Se agrego el tiempo y estilos Widgets

--- Python/Redes_neuroanles.py
+++ Python/Redes_neuroanles.py
@@ -4,6 +4,7 @@ from Perceptron import Perceptron
 from RedNeuronalSimple import RedNeuronalSimple
 from RedNeuronalMulticapa import RedNeuronalMulticapa
 from numpy import array, dot, random
+import time
 
 class RedesNeuronales:
 
@@ -11,9 +12,10 @@ class RedesNeuronales:
         self.ventana = Tk()
         self.ventana.title("Redes Neuronales")
         self.ventana.geometry("600x550")
+        self.ventana.minsize(600, 550)
+        self.ventana.configure(cursor="bogosity")
         self.notebook = ttk.Notebook(self.ventana)
         self.notebook.pack(fill='both',expand='yes')
-        self.ventana.minsize(600, 550)
         self.entradas = {}
         self.salidas = {}
         self.bahias = {}
@@ -24,8 +26,7 @@ class RedesNeuronales:
     
     def createFrame(self, instancia, title):
         self.instances[instancia] = ttk.Frame(self.notebook)
-        self.notebook.add( self.instances.get(instancia), text=title)
-
+        self.notebook.add(self.instances.get(instancia), text=title)
 
     def interface(self, instancia, entradas, descripcion, Ob):
         etiquetaEntradas = []
@@ -35,16 +36,18 @@ class RedesNeuronales:
         self.entradas[instancia] = []
         self.salidas[instancia] = []
 
-        Label(self.instances.get(instancia), text="Ingrese la misma cantidad de item para cada entrada, separados por coma y sin espacio", fg='blue' ).place(x=0 ,y=10)
-        Label(self.instances.get(instancia), text=descripcion.get("nombre")).place(x=10 ,y=60)
-        Label(self.instances.get(instancia), text=descripcion.get("entradas")).place(x=110 ,y=60)
-        Label(self.instances.get(instancia), text=descripcion.get("salidas")).place(x=380 ,y=60)
+        Label(self.instances.get(instancia), text="Ingrese la misma cantidad de item para cada entrada, separados por coma y sin espacio" + "\n"
+        "como se indica en las guias de ejemplo de color VERDE ", fg='black' ).place(x=0 ,y=10)
+        Label(self.instances.get(instancia), text=descripcion.get("nombre"), fg='green', font="Times 14 bold").place(x=5 ,y=60)
+        Label(self.instances.get(instancia), text=descripcion.get("entradas"), fg='green', font="Times 14 bold").place(x=130 ,y=60)
+        Label(self.instances.get(instancia), text=descripcion.get("salidas"), fg='green', font="Times 14 bold").place(x=380 ,y=60)
 
         i = 0
         for i in range(entradas):
             etiquetaEntradas.append(Label(self.instances.get(instancia),text="Entrada # " + str(i) + ' :').place(x=10 ,y=100 + i * 20))
             self.entradas[instancia].append(StringVar())
             nombreEntradas.append(Entry(self.instances.get(instancia), textvariable=self.entradas[instancia][i]).place(x=110,y=100+ i * 20))
+
 
             self.salidas[instancia].append(StringVar())
             etiquetaSalidas.append(Label(self.instances.get(instancia),text="Salida # " + str(i) + ' :').place(x=300 ,y=100 + i * 20))
@@ -63,7 +66,7 @@ class RedesNeuronales:
         etiquetaFuncionActivacion = Label(self.instances.get(instancia),text="Seleccione la Funcion").place(x=10 ,y=100 + (i+4) * 20)
         botonFuncion = ttk.Combobox(self.instances.get(instancia),values=("sigmoide","tangente"),textvariable=self.activacion[instancia]).place(x=200,y=100 + (i+4) * 20)
 
-        botonEntrenar = Button(self.instances.get(instancia),text="Entrenar",fg="black", bg="white", command=lambda: self.callNeurona(instancia)).place(x=0 ,y=100 + (i+7) * 20, height=40, width=550)
+        botonEntrenar = Button(self.instances.get(instancia),text="Entrenar",fg="black", bg="white", font="fixedsys 14 bold", cursor="spider", command=lambda: self.callNeurona(instancia)).place(x=0 ,y=100 + (i+7) * 20, height=40, width=550)
         self.ob[instancia] = Ob
 
     def callNeurona(self, instancia):
@@ -80,15 +83,21 @@ class RedesNeuronales:
                  salidas.append(salida)
 
         ob = self.ob[instancia](entradas, salidas, self.bahias[instancia].get(), self.entrenamiento[instancia].get(), self.activacion[instancia].get())
+
         #INICIA EL TIEMPO
+        inicio =time.time()
         ob.entrenar()
         #FINALIZA EL TIEMPO
+        final=time.time()
+        tiempo=round(final-inicio)
         cadena = ob.imprimirResultado()
-        result = Label(self.instances.get(instancia), text="RESULTADO", fg="red").place(x=0 ,y=100 + (i+9) * 20)
-        message = Label( self.instances.get(instancia), text=cadena, fg='red' ).place(x=0 ,y=100 + (i+10) * 20)
+
+        Label(self.instances.get(instancia), text="RESULTADO", fg="red").place(x=0 ,y=100 + (i+9) * 20)
+        Label( self.instances.get(instancia), text=cadena, fg='red' ).place(x=0 ,y=100 + (i+10) * 20)
+        Label( self.instances.get( instancia ), text="El tiempo que se tardo fue de: " + str(tiempo) + " Segundos", fg='red' ).place( x=0, y=100 + (i + 14) * 20 )
+
         ob.generarGrafico()
 
-        
     def sostenerVentana(self):
         self.ventana.mainloop()
 
